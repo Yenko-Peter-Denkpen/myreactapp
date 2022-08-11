704b0521807218a1cf859168015bad81bf211ea3
Yenko-Peter-Denkpen <110115346+Yenko-Peter-Denkpen@users.noreply.github.com>
2022-08-11 17:48:46 +0000

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div><h1>welcome to my shop</h1>
+
+    <nav>
+    <div>
+
+    <ul>
+    <li>home</li>
+    <li>about</li>
+    <li>product</li>
+    <li>contact</li>
+    </ul>
+    </div>
+    </nav>
+    </div>
+  )
+}
+
+export default NavBar
